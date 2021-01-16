160d458bc858d0645c032b4c1d7c21fe1cbc2150
bashbash96 <amjad.bashiti.96@gmail.com>
2021-01-16 14:38:29 +0200

    Levenstien distance algorithm

---
+++ auto_complete.py
@@ -0,0 +1,8 @@
+
+@dataclass
+class AutoCompleteData:
+    completed_sentence: str
+    source_text: str
+    offset: int
+    score: int
+    # methods that you need to define by yourself
---
+++ string_utils.py
@@ -0,0 +1,53 @@
+def lev_distance(text1, text2):
+    """
+    return the minimum distance between two strings using Levenshtein Algorithm
+    the distance is the number of operations (insertion, deletion, substitution) we
+    need to do to the first string in order to get the second one.
+    :param text1: first string
+    :param text2: second string
+    :return: minimum distance
+    """
+
+    n, m = len(text1), len(text2)
+    # distances = [[j for j in range(m + 1)] for _ in range(n + 1)]
+    #
+    # for row in range(1, n + 1):
+    #     for col in range(1, m + 1):
+    #         # if they are equal take the previous distance.
+    #         if text1[row - 1] == text2[col - 1]:
+    #             distances[row][col] = distances[row - 1][col - 1]
+    #         else:
+    #             # if they are not equal the distance is 1 + the minimum between:
+    #             # 1- insert new letter (row - 1, col)
+    #             # 2- delete letter (row, col - 1)
+    #             # substitute the letter with the other (row - 1, col - 1)
+    #             distances[row][col] = min(distances[row - 1][col - 1],
+    #                                       min(distances[row][col - 1], distances[row - 1][col])) + 1
+    #
+    # return distances[n][m]
+
+    # optimized space to O(m)
+    prev_distances = [i for i in range(m + 1)]
+    curr_distances = []
+    for row in range(1, n + 1):
+        curr_distances = [row]
+        for col in range(1, m + 1):
+            # if they are equal take the previous distance.
+            if text1[row - 1] == text2[col - 1]:
+                curr_distances.append(prev_distances[col - 1])
+            else:
+                # if they are not equal the distance is 1 + the minimum between:
+                # 1- insert new letter (row - 1, col)
+                # 2- delete letter (row, col - 1)
+                # 3- substitute the letter with the other (row - 1, col - 1)
+                min_distance = min(prev_distances[col - 1], min(curr_distances[col - 1], prev_distances[col]))
+                curr_distances.append(min_distance + 1)
+        prev_distances = curr_distances
+
+    return curr_distances[m]
+
+    # Time O(n * m)
+    # Space O(m)
+
+
+print(lev_distance('abc', 'aac'))
